15dcdc1c523abca5046ff531fae0e869f96998ba
unknown <Administrateur@42SE34-1G0455J.ad.campus-eni.fr>
2020-10-08 16:05:48 +0200

    mod 7tp

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -9,91 +9,93 @@ import { OpenTriviaService } from '../providers/openTrivia.provider';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
-  
-  public pseudo : string = 'Janet';
-  public difficulty : string = 'easy';
+
+  public pseudo: string = 'Janet';
+  public difficulty: string = 'easy';
   //validator
-  public isStarted : boolean = false;
-  public isAnswered : boolean = false;
+  public isStarted: boolean = false;
+  public isAnswered: boolean = false;
   public questions = [];
   public questionNumber = 0;
-  public questionDisplayed : any;
+  public questionDisplayed: Question;
   public answers = [];
   public score = 0;
-  public isOver : boolean = false;
-  public colorBtn : string = "tertiary";
-  public attempts : number = 0;
+  public isOver: boolean = false;
+  public attempts: number = 0;
+  public nomQuest = 5;
 
-  constructor(private alertCtrl : AlertController, public toastCtrl: ToastController, private questionProvider : OpenTriviaService) {}
-  
+  constructor(private alertCtrl: AlertController, public toastCtrl: ToastController, private questionProvider: OpenTriviaService) { }
+
   //verifier champs
-  public async start(){
-   
-    if(!this.pseudo || this.pseudo.length <= 3){
+  public async start() {
+
+    if (!this.pseudo || this.pseudo.length <= 3) {
       const alert = await this.alertCtrl.create({
-        header : 'Missing information',
-        message : "Enter a name longer than 3 characters",
-        buttons : ["ok"]
+        header: 'Missing information',
+        message: "Enter a name longer than 3 characters",
+        buttons: ["ok"]
       });
-        alert.present();
-        return;
-    }
-    
-    if(this.difficulty === undefined){
-      const toast = await this.toastCtrl.create({
-        message : 'Please choose a level of difficulty',
-        duration : 3000,
-        color : 'tertiary'
-      });
-        toast.present();
-        return;
+      alert.present();
+      return;
+
     }
 
-    this.isStarted=true;
+    if (this.difficulty === undefined) {
+      const toast = await this.toastCtrl.create({
+        message: 'Please choose a level of difficulty',
+        duration: 3000,
+        color: 'tertiary'
+      });
+      toast.present();
+      return;
+    }
+
+
 
     await this.loadQuestions();
-    this.displayQuestion();
-    
+
+
+    this.isStarted = true;
+
   }
-  
-  private loadQuestions(){
+
+  async loadQuestions() {
     this.isOver = false;
     this.isAnswered = false;
     this.attempts = 0;
     this.score = 0;
     this.questionNumber = 0;
-    this.questionProvider.getQuestions(2, this.difficulty)
-    .then((result)=> {
-      this.questions = result;
-    })
-    .catch(async (err)=>{
+    try {
+      this.questions = await this.questionProvider.getQuestions(this.nomQuest, this.difficulty);
+    } catch (error) {
       const alert = await this.alertCtrl.create({
         header: 'Erreur appele Service',
         message: 'Impossible to recouperate the questions',
         buttons: ['OK']
       });
       alert.present();
-    });
+    }
+    this.displayQuestion();
   }
 
-  public questionAnswered(answer : string){
-    if(this.questionNumber==this.questions.length){
+  public questionAnswered(answer: string) {
+    if (this.questionNumber == this.questions.length) {
       this.isOver = true;
     }
     this.isAnswered = true;
-    if(answer == this.questionDisplayed.correct_answer && this.attempts == 0){
-      this.score ++;
-     }
-    this.attempts ++;
+    if (answer == this.questionDisplayed.correct_answer && this.attempts == 0) {
+      this.score++;
+    }
+    this.attempts++;
   }
-  
-  public nextQuestion(){
+
+  public nextQuestion() {
     this.attempts = 0;
     this.displayQuestion();
     this.isAnswered = false;
   }
 
-  private getAnswers(){
+  private getAnswers() {
     this.answers = this.questionDisplayed.incorrect_answers;
     this.answers.push(this.questionDisplayed.correct_answer);
     this.answers.sort(function () { return 0.5 - Math.random() })
@@ -101,41 +103,50 @@ export class HomePage {
   }
 
   //correction shuffle answers
-  shuffleArray(array){
-    for(var i=array.length -1; i > 0; i--){
-      var j = Math.floor(Math.random() * (i+1));
+  shuffleArray(array) {
+    for (var i = array.length - 1; i > 0; i--) {
+      var j = Math.floor(Math.random() * (i + 1));
       var temp = array[i]
-      array[i] =array [j]
+      array[i] = array[j]
       array[j] = temp;
     }
     return array;
   }
 
-  private displayQuestion(){
+  private displayQuestion() {
 
-      // this.questionDisplayed = this.questions[this.questionNumber];
-      // this.questionDisplayed["answers"] = [];
-      // for(let answer of this.questionDisplayed["incorrect_answers"]){
-      //   this.questionDisplayed["answers"].push({
-      //     answer : answer,
-      //     correct : false
-      //   });
-      // }
+    // this.questionDisplayed = this.questions[this.questionNumber];
+    // this.questionDisplayed["answers"] = [];
+    // for(let answer of this.questionDisplayed["incorrect_answers"]){
+    //   this.questionDisplayed["answers"].push({
+    //     answer : answer,
+    //     correct : false
+    //   });
+    // }
 
-      // this.questionDisplayed["answers"].push({
-      //   answer : this.questionDisplayed["correct_answer"],
-      //   correct : true
-      // });
+    // this.questionDisplayed["answers"].push({
+    //   answer : this.questionDisplayed["correct_answer"],
+    //   correct : true
+    // });
 
-      if(this.questionNumber<this.questions.length){
-        this.questionDisplayed = this.questions[this.questionNumber];
-        this.questionNumber ++;
-        this.getAnswers();
-      }else{
-        //end of game need to change for later
-        this.isOver = true;
-        
-      }
+    if (this.questionNumber < this.questions.length) {
+      this.questionDisplayed = this.questions[this.questionNumber];
+      this.questionNumber++;
+      this.getAnswers();
+    } else {
+      //end of game need to change for later
+      this.isOver = true;
+
+    }
+  }
+  public return() {
+    this.isStarted = false;
+    this.isOver = false;
+    this.isOver = false;
+    this.isAnswered = false;
+    this.attempts = 0;
+    this.questionNumber = 0;
+
   }
 
 }
--- src/app/providers/openTrivia.provider.ts
+++ src/app/providers/openTrivia.provider.ts
@@ -1,41 +1,62 @@
+import { HttpClient, HttpParams } from '@angular/common/http';
 import { Injectable } from "@angular/core";
 import { Question } from '../models/questions';
 
 @Injectable({
     //can declare it here instead of the app.module
-    providedIn:'root'
+    providedIn: 'root'
 })
 export class OpenTriviaService {
 
-    public getQuestions(questions: number, difficultyLevel: string):
+    constructor(private httpClient : HttpClient){}
+
+   async getQuestions(questions: number, difficultyLevel: string):
+
+
         Promise<Array<Question>> {
         return new Promise((resolve, reject) => {
-            resolve([
-                {
-                    category: "Entertainment: Japanese Anime & Manga",
-                    type: "multiple", 
-                    difficulty: "easy",
-                    question: "In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?",
-                    correct_answer: "The Salamander",
-                    incorrect_answers: ["The Dragon Slayer", "The Dragon", "The Demon"]
-                },
-                {
-                    category: "Entertainment: Japanese Anime & Manga",
-                    type: "multiple", 
-                    difficulty: "easy",
-                    question: "Which months have 28 days?",
-                    correct_answer: "all of them",
-                    incorrect_answers: ["February", "March", "September"]
-                },
-                {
-                    category: "Entertainment: Video Games",
-                    type: "boolean", 
-                    difficulty: "medium",
-                    question: "&quot;Returnto Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
-                    correct_answer: "False",
-                    incorrect_answers: ["True"]
+
+            let params = new HttpParams();
+            params = params.append('amount', questions.toString());
+            params = params.append('difficulty', difficultyLevel);
+            this.httpClient.get('https://opentdb.com/api.php', {params:params})
+            .toPromise()
+            .then((response)=> {
+                if(response && response['response_code'] == 0 && response['results']){
+                    resolve(response['results']);
+                }else {
+                    reject("The server is not responding");
                 }
-            ]);
+            })
+            .catch((error)=> {
+                reject(error);
+            });
+            //     resolve([
+            //         {
+            //             category: "Entertainment: Japanese Anime & Manga",
+            //             type: "multiple", 
+            //             difficulty: "easy",
+            //             question: "In &quot;Fairy Tail&quot;, what is the nickname of Natsu Dragneel?",
+            //             correct_answer: "The Salamander",
+            //             incorrect_answers: ["The Dragon Slayer", "The Dragon", "The Demon"]
+            //         },
+            //         {
+            //             category: "Entertainment: Japanese Anime & Manga",
+            //             type: "multiple", 
+            //             difficulty: "easy",
+            //             question: "Which months have 28 days?",
+            //             correct_answer: "all of them",
+            //             incorrect_answers: ["February", "March", "September"]
+            //         },
+            //         {
+            //             category: "Entertainment: Video Games",
+            //             type: "boolean", 
+            //             difficulty: "medium",
+            //             question: "&quot;Returnto Castle Wolfenstein&quot; was the only game of the Wolfenstein series where you don&#039;t play as William &quot;B.J.&quot; Blazkowicz.",
+            //             correct_answer: "False",
+            //             incorrect_answers: ["True"]
+            //         }
+            //     ]);
 
         });
     }
